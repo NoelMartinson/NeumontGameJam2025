3e37530548aae9a11d6a9eaa7fc4c09935034afa
ScareCellar <98201877+ScareCellar@users.noreply.github.com>
2025-08-08 13:55:57 -0600

    updated Folder.h

--- Source/Game/Folder.h
+++ Source/Game/Folder.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Core/StringHelper.h"
+#include <vector>
+#include <memory>
 
 class Folder {
 public:
@@ -8,7 +10,23 @@ public:
 		_name =  name;
 	}
 
+	bool AddFolder(Folder addedFolder) {
+		bool yes = false;
+		for (Folder folder : _folders) {
+			if (folder._name == addedFolder._name) yes = true;
+		}
+		if (yes) {
+			_folders.push_back(addedFolder);
+			
+		}
+		return yes;
+	}
+
 
 private:
 	std::string _name;
+	std::vector<Folder> _folders;
+
+	
+
 };
