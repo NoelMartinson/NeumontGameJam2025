8717c5a978506879fa67d4ca33cade5b8cac2150
ScareCellar <98201877+ScareCellar@users.noreply.github.com>
2025-08-08 14:07:55 -0600

    made small updates  to Folder.h

--- Source/Game/Folder.h
+++ Source/Game/Folder.h
@@ -6,8 +6,9 @@
 class Folder {
 public:
 	Folder() = default;
-	Folder(std::string name) {
+	Folder(std::string name, bool isEncrypted) {
 		_name =  name;
+		_encrypted = isEncrypted;
 	}
 
 	bool AddFolder(Folder addedFolder) {
@@ -17,15 +18,17 @@ public:
 		}
 		if (yes) {
 			_folders.push_back(addedFolder);
-			
 		}
 		return yes;
 	}
 
+	
 
 private:
 	std::string _name;
 	std::vector<Folder> _folders;
+
+	bool _encrypted;
 
 	
 
